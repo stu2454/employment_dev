0efa8b66b41483c310d7b763fc705bc3011fbd5a
Stuart Smith <stu2038@me.com>
2025-01-07 22:24:28 +1100

    Update database connection for Render Postgresql

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,21 @@
+const { reject } = require('bcrypt/promises');
 const { Client } = require('pg');
 
 const createClient = () => {
-  return new Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-  });
+
+  const connectionString = process.env.DATABASE_URL;
+
+  //return new Client({
+    //user: process.env.DB_USER,
+    //host: process.env.DB_HOST,
+    //database: process.env.DB_NAME,
+    //password: process.env.DB_PASSWORD,
+    //port: process.env.DB_PORT,
+  //});
+ return new Client({
+  connectionString: connectionString,
+  ssl: connectionString ? { rejectUnauthorized: false } : false, //Use SSL for Render
+});
 };
 
 module.exports = createClient;
